3b9aedc3a4c815dcbf2496577feeb608b569b12c
msjun <msjuns@gmail.com>
2022-10-21 15:30:15 +0900

    test

--- src/test/java/com/mansung/calculator/CalculatorTest.java
+++ src/test/java/com/mansung/calculator/CalculatorTest.java
@@ -3,15 +3,12 @@ package com.mansung.calculator;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
-import com.mansung.calculator.Calculator;
-
-
 public class CalculatorTest {
     private Calculator calculator = new Calculator();
     
     @Test 
     public void testSum() {
-        assertEqauls(5, calculator.sum(2,3));
+        assertEquals(5, calculator.sum(2,3));
        
     }
 
